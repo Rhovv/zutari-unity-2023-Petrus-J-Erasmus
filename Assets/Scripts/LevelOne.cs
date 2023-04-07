fbda0a651459dad67e351991bb0c4333205c6ded
Petrus J Erasmus <petrusjerasmus@gmail.com>
2023-04-07 17:23:58 +0200

    Implemented the Level One scene, with cube movement and recoloring

--- Assets/Scripts/LevelOne.cs
+++ Assets/Scripts/LevelOne.cs
@@ -1,18 +1,130 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class LevelOne : MonoBehaviour
 {
+    public Color colStill;
+    public Color colUp;
+    public Color colDown;
+    public Color colLeft;
+    public Color colRight;
+
+    public Rigidbody cube;
+    public BoxCollider box;
+    public Material matCube;
+
+    public TMP_InputField inSpeed;
+
+    Vector3 direction = Vector3.zero;
+    Vector3 velocity;
+    Vector3 position;
+    Vector3 minPos;
+    Vector3 maxPos;
+    int speed = 100;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        inSpeed.text = speed.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        direction.x = 0f;
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction.x -= 1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction.x += 1f;
+        }
+
+        direction.y = 0f;
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction.y += 1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction.y -= 1f;
+        }
+
+        position = cube.transform.position;
+        minPos = position - box.size / 2f;
+        maxPos = position + box.size / 2f;
+        if ((Camera.main.WorldToScreenPoint(maxPos).x < 0f) || (Camera.main.WorldToScreenPoint(minPos).x > Display.main.renderingWidth))
+        {
+            position.x = -position.x;
+        }
+        if ((Camera.main.WorldToScreenPoint(maxPos).y < 0f) || (Camera.main.WorldToScreenPoint(minPos).y > Display.main.renderingHeight))
+        {
+            position.y = -position.y;
+        }
+        cube.position = position;
+    }
+
+    private void FixedUpdate()
+    {
+        //velocity = cube.velocity;
+        //if (direction.x == 0f)
+        //{
+        //    velocity.x = 0f;
+        //}
+        //if (direction.y == 0f)
+        //{
+        //    velocity.y = 0f;
+        //}
+
+        cube.AddForce(direction * speed * Time.deltaTime);
+
+        if (cube.velocity.y != 0)
+        {
+            if ((cube.velocity.x != 0) && (Mathf.Abs(cube.velocity.x) > Mathf.Abs(cube.velocity.y)))
+            {
+                if (cube.velocity.x > 0) { matCube.color = colRight; }
+                else { matCube.color = colLeft; }
+            }
+            else
+            {
+                if (cube.velocity.y > 0) { matCube.color = colUp; }
+                else { matCube.color = colDown; }
+            }
+        }
+        else
+        {
+            if (cube.velocity.x > 0) { matCube.color = colRight; }
+            else if (cube.velocity.x < 0) { matCube.color = colLeft; }
+            else { matCube.color = colStill; }
+        }
+    }
+
+    public void inSpeed_endEdit(string s)
+    {
+        if ((s == null) || (s == ""))
+        {
+            inSpeed.text = speed.ToString();
+            return;
+        }
+
+        speed = int.Parse(s);
+        if (speed < 0)
+        {
+            speed = 0;
+        }
+        if (speed > 1000)
+        {
+            speed = 1000;
+        }
+        inSpeed.text = speed.ToString();
+    }
+
+    public void btnMenu_click(string s)
+    {
+        Controller.loadScene(s);
     }
 }
--- README.md
+++ README.md
@@ -14,3 +14,14 @@ I created a script for each of the different scenes to handle their own overall 
 
 The scene buttons call a load scene function on Controller, passing through the name of the required scene. I also added an Exit button that either stops play while in the editor, or quits the application if in a built version.
 
+Next I started implementing the Level One scene. I set the camera to orthographic as required, and added and centered the cube. I also created a material to add to the cube in order to change it's color as required, and a rigidbody component to interact with as specified.
+
+I removed gravity (on the rigidbody) and added some drag so that the cube slides to a stop after the buttons have been released.
+
+In the script for the cube controls I apply a force to the cube while the keys are pressed. The force is applied in the FixedUpdate function to ensure it works the same regardless of the framerate.
+
+On update, I check the velocity of the cube and switch the cube's color according to the direction it is travelling the fastest in. I added a small color compass to show the colors for the specific directions.
+
+I added a bit of UI to allow a return to the main menu, and to view and change the cube speed. I also added some limits to ensure the speed doesn't go below 0 or above a 1000.
+
+As to the cube reappearing on the opposite side of the screen, I added a box-collider which includes a size component that I use in conjuction with the cube's position to determine its position on screen (with WorldToScreenPoint) and see if it has passed off of the screen. When it does, I simply position it on the opposite side of where it left the screen, thus bringing it back into view.
